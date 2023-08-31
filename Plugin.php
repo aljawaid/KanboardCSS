be22e1237bcf9a0286be556c3b14fb65465ee543
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-31 02:36:53 +0100

    FIX: Update Plugin Description to Mention FontSwitcher

--- Plugin.php
+++ Plugin.php
@@ -28,7 +28,7 @@ class Plugin extends Base
 
     public function getPluginDescription()
     {
-        return t('This pure CSS theme replaces the look and feel of the Kanboard user interface. For modern browsers using gradients and shades of blue and red, KanboardCSS refreshes the the user experience. A new animated notification icon easily identifies new messages without affecting browser performance. All tables and forms are restyled to save screen space and improve productivity. This theme works perfectly with other plugins.');
+        return t('This pure CSS theme replaces the look and feel of the Kanboard user interface. For modern browsers using gradients and shades of blue and red, KanboardCSS refreshes the the user experience. A new animated notification icon easily identifies new messages without affecting browser performance. All tables and forms are restyled to save screen space and improve productivity. Install with FontSwitcher to give a complete new overhaul to your application.');
     }
 
     public function getPluginAuthor()
